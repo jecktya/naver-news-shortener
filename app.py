bbade1944804d83dc2238ad279ce07e4df707f16
jecktya <jecktya@gmail.com>
2025-06-29 16:47:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import asyncio
 from playwright.async_api import async_playwright
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 TEMPLATE_KWS = [
