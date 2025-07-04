e5ad065fe697015469937844c6e867d03e81f8e6
jecktya <jecktya@gmail.com>
2025-07-04 19:28:52 +0900

    Update news_analyzer.py

--- news_analyzer.py
+++ news_analyzer.py
@@ -1,2 +1,10 @@
-pip install requests beautifulsoup4
-python news_analyzer.py "https://m.search.naver.com/search.naver?ssc=tab.m_news.all&query=육군%20|%20국방%20|%20외교%20|%20안보%20|%20북한%20|%20신병%20|%20교육대%20|%20훈련%20|%20간부%20|%20장교%20|%20부사관%20|%20병사%20|%20용사%20|%20군무원&sm=mtb_opt&sort=1&photo=0&field=0&pd=-1&ds=2025.06.29&de=2025.06.29"
+from fastapi import FastAPI, Query
+from news_analyzer import fetch_html, parse_newslist
+
+app = FastAPI()
+
+@app.get("/analyze")
+async def analyze(url: str = Query(..., description="네이버 모바일 뉴스 URL")):
+    html = fetch_html(url)
+    articles = parse_newslist(html)
+    return {"count": len(articles), "articles": articles}
