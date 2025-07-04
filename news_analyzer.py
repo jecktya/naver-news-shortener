5fdb2c15c99e853eca604a4dd7534f72bcff7a7e
jecktya <jecktya@gmail.com>
2025-07-04 19:20:10 +0900

    Create  news_analyzer.py

---
+++ news_analyzer.py
@@ -0,0 +1,2 @@
+pip install requests beautifulsoup4
+python news_analyzer.py "https://m.search.naver.com/search.naver?ssc=tab.m_news.all&query=육군%20|%20국방%20|%20외교%20|%20안보%20|%20북한%20|%20신병%20|%20교육대%20|%20훈련%20|%20간부%20|%20장교%20|%20부사관%20|%20병사%20|%20용사%20|%20군무원&sm=mtb_opt&sort=1&photo=0&field=0&pd=-1&ds=2025.06.29&de=2025.06.29"
